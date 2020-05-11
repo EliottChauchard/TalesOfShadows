71e76cf8bb70c200f075a019e7b0ee6f00b5df27
Keanu <keanunohea@gmail.com>
2020-05-11 11:43:45 +0200

    Rescale
    Ta mêrè la personne gentille Eliott <3

---
+++ Assets/_Scripts/KNF_ParallaxLayer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class KNF_ParallaxLayer : MonoBehaviour
+{
+    [SerializeField] float multiplier_x = 0.0f;
+    [SerializeField] float multiplier_y = 0.0f;
+    [SerializeField] bool horizontalOnly = true;
+
+    private Transform cameraTransform;
+
+    private Vector3 startCameraPos;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        startCameraPos = cameraTransform.position;
+        startPos = transform.position;
+    }
+
+
+    private void LateUpdate()
+    {
+        var position = startPos;
+        if (horizontalOnly)
+            position.x += multiplier_x * (cameraTransform.position.x - startCameraPos.x);
+        else
+            position.x += multiplier_x * (cameraTransform.position.x - startCameraPos.x);
+            position.y += multiplier_y * (cameraTransform.position.y - startCameraPos.y);
+
+        transform.position = position;
+    }
+
+}

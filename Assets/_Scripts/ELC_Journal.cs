87a2731459d2f327a88106dbc01654ec84675add
Keanu <keanunohea@gmail.com>
2020-06-04 01:01:17 +0200

    Journaux + Histoire + Modifs LD

--- Assets/_Scripts/ELC_Journal.cs
+++ Assets/_Scripts/ELC_Journal.cs
@@ -7,14 +7,54 @@ public class ELC_Journal : MonoBehaviour
     [SerializeField]
     public int refNumber;
     private int numberSaved;
-    
+    public GameObject pieceJournal;
+    private bool isTaken;
+    private bool hasleft;
+    public float dissapearTime = 0.2f;
+
+    private void Start()
+    {
+        pieceJournal.SetActive(false);
+        this.gameObject.SetActive(true);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isTaken = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            hasleft = true;
+        }
+    }
+
+
     void Update()
     {
         numberSaved = PlayerPrefs.GetInt("Collectible" + refNumber);
 
-        if(numberSaved == 1)
+        if (isTaken == true)
         {
-            this.gameObject.SetActive(false);
+            pieceJournal.SetActive(true);
+            StartCoroutine(ShowingPJ());
         }
+    }
+
+    IEnumerator ShowingPJ()
+    {
+        if (hasleft == true)
+        {
+            yield return new WaitForSeconds(dissapearTime);
+            pieceJournal.SetActive(false);
+            this.gameObject.SetActive(false);
+            yield return null;
+        }
+
     }
 }

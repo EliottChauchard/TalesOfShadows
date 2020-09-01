a34a8113c0eaa9c230d099831d3d5e900f81d1db
Keanu <keanunohea@gmail.com>
2020-09-01 19:02:13 +0200

    LD3 ND

---
+++ Assets/_Scripts/KNF_DialogueAppear.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KNF_DialogueAppear : MonoBehaviour
+{
+    public GameObject dialogueLines;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueLines.SetActive(false);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            dialogueLines.SetActive(true);
+            Debug.Log("yes");
+        } 
+    }
+}
